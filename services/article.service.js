f6a7f69a7715ae6bfb975bb26846a8f14ddec09d
phuczs <phuchasprovt@gmail.com>
2024-12-07 18:42:11 +0700

    cook article contents

--- services/article.service.js
+++ services/article.service.js
@@ -4,4 +4,19 @@ export default{
     findAll(){
         return db('articles');
     },
+    findById(id) {
+        return db('articles').where('NewsID', id).first();
+      },
+    
+    findByCatId(catId) {
+        return db('articles').where('CatID', catId);
+      },
+    
+    findPageByCatId(catId, limit, offset) {
+        return db('articles').where('CatID', catId).limit(limit).offset(offset);
+      },
+    
+      countByCatId(catId) {
+        return db('articles').where('CatID', catId).count('* as total').first();
+      },
 }
