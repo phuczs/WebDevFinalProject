cc67779ea62c940234c4e600afd35b4ce2a0343b
phuczs <phuchasprovt@gmail.com>
2024-12-08 14:54:55 +0700

    them forgot password

--- services/user.service.js
+++ services/user.service.js
@@ -1,5 +1,5 @@
 import db from '../utils/db.js';
-
+import bcrypt from 'bcryptjs';
 export default {
     // Find user by username
     findByUsername(username) {
@@ -44,5 +44,36 @@ export default {
             throw new Error('Error updating password');
         }
     },
+
+    async findUserByEmail(email) {
+        return db('users').where('email', email).first();
+    },
     
+    async saveOTP(email, otp) {
+        return db('users')
+            .where('email', email)
+            .update({
+                otp: otp,
+                otp_expiry: db.raw('DATE_ADD(NOW(), INTERVAL 15 MINUTE)')
+            });
+    },
+    
+    async verifyOTP(email, otp) {
+        return db('users')
+            .where('email', email)
+            .where('otp', otp)
+            .where('otp_expiry', '>', db.fn.now())
+            .first();
+    },
+    
+    async updatePasswordWithEmail(email, newPassword) { //Renamed to avoid conflict
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        return db('users')
+            .where('email', email)
+            .update({
+                password: hashedPassword,
+                otp: null,
+                otp_expiry: null
+            });
+    }
 };
