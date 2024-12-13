a22887695271c0efddedbc757a591c8758b36315
phuczs <phuchasprovt@gmail.com>
2024-12-13 15:14:27 +0700

    them comment

--- routes/article-user.route.js
+++ routes/article-user.route.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import articleService from '../services/article.service.js';
 import commentService from '../services/comment.service.js';
+import { isAuth } from '../middlewares/auth_mdw.js';
+import { format } from 'date-fns';
 
 const router = express.Router();
 
@@ -39,19 +41,47 @@ router.get('/byCat', async function (req, res) {
 
   
 
-  // router.get('/comment', async function (req, res) {
-  //   // const comment = await commentService.findAllComments();
-  //   res.render('vwArticle/comment');
-  //   //   list:list,
-  //   // });
-  // });
+  router.get('/comment', isAuth, async function (req, res) {
+    try {
+        const articleId = req.query.id;
+        const comments = await commentService.findCommentsByNewsId(articleId);
+        const article = await articleService.findById(articleId);
+        
+        res.render('vwArticle/comment', { 
+            comments: comments,
+            user: req.session.authUser,
+            article: article
+        }); 
+    } catch (error) {
+        res.status(500).send('Error fetching comments');
+    }
+  });
 
-  // // Handle new comment submission
-  // router.post('/comment', async function (req, res) {
-  //   // console.log(req.body);
-  //   const ret = await commentService.add(req.body);
-  //   // console.log(ret); // insertId
-  //   res.render('vwArticle/');
-  // });
+  // Handle new comment submission
+  router.post('/comment', isAuth, async function (req, res) {
+    try {
+        const commentData = {
+            newsID: req.query.id,
+            content: req.body.content,
+            author: req.session.authUser.name,
+        };
+
+        const commentId = await commentService.addComment(commentData);
+        
+        // After adding comment, fetch comments again to display updated list
+        const comments = await commentService.findCommentsByNewsId(req.body.newsID);
+        comments.forEach(comment => {
+            comment.date_create = format(new Date(comment.date_create), 'MMMM do, yyyy H:mm:ss'); // Format the date
+        });
+        
+        res.render('vwArticle/comment', {
+            successMessage: 'Comment published successfully!',
+            comments: comments,
+            user: req.session.authUser
+        });
+    } catch (error) {
+        res.status(500).send('Error publishing comment');
+    }
+  });
   
   export default router;
